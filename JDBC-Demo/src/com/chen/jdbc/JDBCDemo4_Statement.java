459d1cdaf159e3e3d36dde4c29730c0da3db71bd
lingtiao <3010679226@qq.com>
2022-06-28 16:46:48 +0800

    2022年6月28日16点46分

---
+++ JDBC-Demo/src/com/chen/jdbc/JDBCDemo4_Statement.java
@@ -0,0 +1,40 @@
+package com.chen.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * JDBC API 详解：Connection
+ */
+public class JDBCDemo4_Statement {
+
+
+    public void testDML() throws SQLException {
+       //1. 注册驱动
+       //Class.forName("com.mysql.jdbc.Driver");
+       //2. 获取连接：如果连接的是本机mysql并且端口是默认的 3306 可以简化书写
+       String url = "jdbc:mysql:///db1?useSSL=false";
+       String username = "root";
+       String password = "1234";
+       Connection conn = DriverManager.getConnection(url, username, password);
+       //3. 定义sql
+       String sql = "update account set money = 5000 where id = 1";
+       //4. 获取执行sql的对象 Statement
+       Statement stmt = conn.createStatement();
+       //5. 执行sql
+       int count = stmt.executeUpdate(sql);//受影响的行数
+       //6. 处理结果
+       //System.out.println(count);
+       if (count>0){
+          System.out.println("Ture");
+       }
+       else{
+          System.out.println("False");
+       }
+       //7. 释放资源
+       stmt.close();
+       conn.close();
+   }
+}
